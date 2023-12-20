be01021de37ac88e01694285ca59e795ff4d9b7f
giuliabaratella <giuliabaratella7@gmail.com>
2023-12-20 14:57:31 +0100

    added movies data + movies index content

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,7 +4,27 @@
 
 @section('content')
     <main>
-        <h1>Movies</h1>
+        <div class="container">
+            <h1>Movies</h1>
+
+            <div class="row row-gap-4">
+                @foreach ($movies as $movie)
+                    <div class="col-12 col-md-4 col-lg-3">
+                        <div class="card">
+                            <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie->title }}</h5>
+                                <h6 class="card-title">{{ $movie->original_title }}</h6>
+
+                                <p class="card-text">Language: {{ $movie->language }}</p>
+
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+        </div>
 
     </main>
 
